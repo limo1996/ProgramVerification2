120ab4393f2a70172768c9bfa82bb5cd3bed4263
Jakub Lichman <lichman.jakub@gmail.com>
2018-04-21 23:44:48 +0200

    Code commented in MyVerifier

--- VerifierProject/src/main/scala/core/MyVerifier.scala
+++ VerifierProject/src/main/scala/core/MyVerifier.scala
@@ -194,51 +194,62 @@ class MyVerifier(private val logger: Logger) extends BareboneVerifier {
     viperResult
   }
 
+  /*
+  * Assumes seqn is body of method that is already converted in DSA and contains only
+  * {assert, assume, if(with appended assumes as condition in both branches)} and pre and post conditions appended
+  * as first respectively last statements.
+  * */
   def wlp(seqn: Seqn) : ListBuffer[sil.Exp] = {
     var delta = new ListBuffer[sil.Exp]()
     delta = wlp_star_seq(seqn, delta)
     delta
   }
 
+  /*
+  * Creates delta set from sequence. Takes existing delta set as parameter and returns new one.
+  * */
   def wlp_star_seq(seqn: Seqn,  delta : ListBuffer[sil.Exp]) : ListBuffer[sil.Exp] = seqn match {
     case Seqn(statements, variableDeclarations) => {
-      println("case seqn: " + statements)
+      // println("case seqn: " + statements)
       // Viper sequential composition (which can be treated as a scope) takes a Scala
       // Seq of Stmt elements and variable declarations.
       var new_delta = delta
       for(stmt <- statements.reverse){
         new_delta = wlp_star(stmt, new_delta)
       }
-      println("delta: " + new_delta)
+      // println("delta: " + new_delta)
       new_delta
     }
     case _ => throw new Exception("wpl_wrapper: no case matched with : " + seqn)
   }
 
+  /*
+  * Converts statement according to previous delta set and returns new one.
+  * */
   def wlp_star(stmt : sil.Stmt,  delta : ListBuffer[sil.Exp]) : ListBuffer[sil.Exp] = {
     stmt match {
-      case sil.Assert(expression) => {
-        println("case assert: " + expression)
+      case sil.Assert(expression) => {              // in assert we just append to set
+        //println("case assert: " + expression)
         var new_delta = delta
         new_delta += expression
-        println("delta: " + new_delta)
+        //println("delta: " + new_delta)
         new_delta
       }
-      case Inhale(expression) => {
-        println("case assume: " + expression)
+      case Inhale(expression) => {                  // in assume we add A1 => A for every A from delta where A1 is expression in assume
+        //println("case assume: " + expression)
         var new_delta = new ListBuffer[sil.Exp]()
         for(expr <- delta){
           new_delta += sil.Implies(expression, expr)()
         }
-        println("delta: " + new_delta)
+        //println("delta: " + new_delta)
         new_delta
       }
-      case If(condition, ifTrue, ifFalse) => {
-          println("case if: " + condition)
+      case If(condition, ifTrue, ifFalse) => {      // in if we just simply convert both blocks from delta and new delta is union of the two
+          //println("case if: " + condition)
           var new_delta = new ListBuffer[sil.Exp]()
           new_delta ++= wlp_star_seq(ifTrue, delta)
           new_delta ++= wlp_star_seq(ifFalse, delta)
-          println("delta: " + new_delta)
+          //println("delta: " + new_delta)
           new_delta
       }
       case _ => throw new Exception("wpl_star: no case matched with : " + stmt)
